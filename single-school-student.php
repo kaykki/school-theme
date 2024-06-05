ede9b3de6693895bc7af4b51e2fbdc612c7383b1
Kaki Kagatan <kakikagatan@outlook.com>
2024-06-04 22:15:53 -0700

    improved student section styling

--- single-school-student.php
+++ single-school-student.php
@@ -17,17 +17,6 @@ get_header();
 			the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );
-
-			$id = get_the_ID();
-			?>
-			<pre><?php get_the_ID(); ?><pre>
-			<?php
-			// the_post_navigation(
-			// 	array(
-			// 		'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'school-theme' ) . '</span> <span class="nav-title">%title</span>',
-			// 		'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'school-theme' ) . '</span> <span class="nav-title">%title</span>',
-			// 	)
-			// );
 		endwhile; // End of the loop.
 		?>
 
